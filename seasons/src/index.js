f1ef119dc616b05d23999ba85b17c575ca1765c7
Avik Khamaru <tubla14@gmail.com>
2020-10-25 07:55:41 +0530

    Class Components

---
+++ seasons/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Component{
+    
+    constructor(props){
+        super(props);
+        this.state={lat: null};
+       
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({lat: position.coords.latitude});
+            },
+            (err) => console.log(err)
+        );
+    }
+
+    render(){
+       
+        return <div>Latitude : {this.state.lat}</div>
+    }
+}
+
+ReactDom.render(<App/>,document.querySelector('#root'));
+
